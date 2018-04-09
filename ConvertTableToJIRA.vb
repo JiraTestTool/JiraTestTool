f756ddb3bfc2e9af3f3ffe5e5b1e71da8b936a18
Michael Fekadu <mfekadu@users.noreply.github.com>
2018-04-08 17:48:49 -0700

    implemented bolding via keywords
    Realized I was going down the wrong path by trying to read each individual character and the .FontStyle property. While, that may be needed for reading the current format in the Spreadsheet ... it's tough to keep track of the indices.
    
    For the sake of time and the design requirements, the keyword formatting will suffice since customized formatting would be a "nice-to-have"
    
    CHANGES:
    * added `Dim boldArray(20) As String`
    ** the `20` is an arbitrary magic number for now
    * added `IsInArray()` to find strings within a string array.
    ** thanks to https://stackoverflow.com/q/11109832/5411712
    * added code to split the string value of each Cell using the space character as a delimeter .... outputs to cellWordsArr
    * added a loop over the cellWordsArr to add the '*' appropriately
    ** this is the BOLD code to be refactored
    
    TODO:
    * refactor BOLD code into a General Format Function that accepts Italics / Underlined / Bold as parameters
    
    * make 2 more arrays called `italicsArray` and `underlinedArray`
    
    *

--- ConvertTableToJIRA.vb
+++ ConvertTableToJIRA.vb
@@ -1,5 +1,12 @@
-' from https://gist.github.com/DBremen/0ba67c6ec894ee581d98
 
+' https://stackoverflow.com/q/11109832/5411712
+
+Function IsInArray(stringToBeFound As String, arr As Variant) As Boolean
+  IsInArray = (UBound(Filter(arr, stringToBeFound)) > -1)
+End Function
+
+
+' initial inspiration : https://gist.github.com/DBremen/0ba67c6ec894ee581d98
 Sub ConvertToJiraTable()
     Dim workingRange As Range, currCol As Range, currRow As Range
     Dim rowIndex As Long, colIndex As Long
@@ -36,6 +43,22 @@ Sub ConvertToJiraTable()
     stringToBold = ""
     Dim currChar As String
 
+    boldedCellVal = ""
+
+    Dim boldArray(20) As String
+    boldArray(0) = "Click"
+    boldArray(1) = "Navigate"
+    boldArray(2) = "Open"
+    boldArray(3) = "Save"
+    boldArray(4) = "Load"
+    boldArray(5) = "Delete"
+    boldArray(6) = "Hello"
+    boldArray(7) = "Set"
+    boldArray(8) = "Fill"
+    boldArray(9) = "Enter"
+    boldArray(10) = "Drag"
+    boldArray(11) = "Type"
+
     Application.ScreenUpdating = False
 
     Set workingRange = Range("A1").CurrentRegion
@@ -43,99 +66,175 @@ Sub ConvertToJiraTable()
         colIndex = 1
         For Each currCol In currRow.Columns
 
-            ' read each char in a range
-            For i = 1 To Len(Cells(rowIndex, colIndex).Value)
-                If Cells(rowIndex, colIndex).Characters(i, 1).Font.FontStyle = "Bold" Then
-                    currChar = Cells(rowIndex, colIndex).Characters(i, 1).Text
+            cellStr = currCol.Value
 
-                    If arrayCounter >= 5000 Then
-                        Debug.Print ("*Someone fit 5000 individually Bolded strings into a Cell! Wow!*")
-                        Exit Sub
-                    End If
+            ' according to https://techsupport.osisoft.com/Troubleshooting/KB/KB01372
+            ' There can be weird non ASCII spaces due to copy-pasting from an HTML source
+            ' The werid characters mess with the Split() function.
+            ' The character is Char(160) or "%C2%A0"
+            ' this replacement converts the space character to ASCII
+            cellStr = Replace(cellStr, CStr(Chr(160)), CStr(Chr(32)))
+            ' then use the space character as the delimiter
+            ' because JIRA only formats words separated by space.
+            cellWordsArr = Split(cellStr, " ")
 
-                    If foundBoldStart = False Then
-                        foundBoldStart = True
-                        ' because we just found a Bold word,
-                        ' we want to remember the start_index and the end_index
-                        start_index_of_stringToBold = i
-                        end_index_of_stringToBold = i
-''                    If foundBoldStart = False Then
-''                        foundBoldStart = True
-''                        ' because we just found a Bold word,
-''                        ' we want to remember the start_index and the end_index
-''                        start_index_of_stringToBold = i
-''                        end_index_of_stringToBold = i
-                    End If
-                    If foundBoldStart = True Then
-                        stringToBold = stringToBold & currChar
-                        ' because the Bold word was previously found
-                        ' we want to keep track of the end index
-                        end_index_of_stringToBold = i
-                        stringsToBoldArray(arrayCounter, 0) = stringToBold
-                        stringsToBoldArray(arrayCounter, 1) = start_index_of_stringToBold
-                        stringsToBoldArray(arrayCounter, 2) = end_index_of_stringToBold
-                    End If
-                    'Debug.Print (stringToBold)
-                Else
-                  ' The bold word ended before the end of the string
-                  ' lets reset out variables and prepare for the next Bold word
-                  If stringToBold <> "" Then
-''                      Debug.Print ("*" & stringsToBoldArray(arrayCounter, 0) & "*")
-''                      Debug.Print ("start = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(arrayCounter, 1), 1).Text)
-''                      Debug.Print ("end   = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(arrayCounter, 2), 1).Text)
-''                      Debug.Print ("diff  = " & stringsToBoldArray(arrayCounter, 2) - stringsToBoldArray(arrayCounter, 1))
-                      foundBoldStart = False ' reset to False until next bold
-                      arrayCounter = arrayCounter + 1
-                      stringToBold = ""      ' reset to empty string until next bold
-                      start_index_of_stringToBold = 0
-                      end_index_of_stringToBold = 0
-                  End If
+            For i = LBound(cellWordsArr) To UBound(cellWordsArr)
+                Debug.Print (cellWordsArr(i))
+
+                ' the empty string exists in cellWordsArr
+                ' avoid empty string and match the keywords
+                If cellWordsArr(i) <> "" _
+                    And IsInArray(CStr(cellWordsArr(i)), boldArray) Then
+                    cellWordsArr(i) = "*" & cellWordsArr(i) & "*"
                 End If
-                'Debug.Print (currChar)
-            Next i
-''            If stringToBold <> "" Then
-''                ' This if condition is useful when the Bold word
-''                ' ends as the final word in the string
-''                ' e.g: *Click* the button, *quickly!*
-''                Debug.Print ("*" & stringToBold & "*")
-''                Debug.Print ("start = " & Cells(rowIndex, colIndex).Characters(start_index_of_stringToBold, 1).Text)
-''                Debug.Print ("end   = " & Cells(rowIndex, colIndex).Characters(end_index_of_stringToBold, 1).Text)
-''                Debug.Print ("diff  = " & end_index_of_stringToBold - start_index_of_stringToBold)
-''                foundBoldStart = False ' reset to False until next bold
-''                stringToBold = ""      ' reset to empty string until next bold
-''                start_index_of_stringToBold = 0
-''                end_index_of_stringToBold = 0
-''            End If
 
-            'For i = 1 To Len(Range("A1").Value)
-            '  If Range("A1").Characters(i, 1).Font.FontStyle = "Bold" Then
-            '    If foundBoldStart = False Then
-            '        foundBoldStart = True
-            '    End If
-            '    If foundBoldStart = True Then
-            '        stringToBold = stringToBold & Range("A1").Characters(i, 1).Text
-            '    End If
-            '    Debug.Print (stringToBold)
-            '
-            '    'Debug.Print ("The " & Range("A1").Characters(i, 1).Text & " character is bold.")
-            '  End If
-            'Next i
-
-
-            'replace empty value with space to generate the cell border
-            cellVal = currCol.Value
-
-            For i = 0 To arrayCounter
-                ' iterate over "stringsToBoldArray" and modify "cellVal" with '*' appropriately
-                Debug.Print ("*" & stringsToBoldArray(i, 0) & "*")
-                Debug.Print ("start = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(i, 1), 1).Text)
-                Debug.Print ("end   = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(i, 2), 1).Text)
-                Debug.Print ("diff  = " & stringsToBoldArray(arrayCounter, 2) - stringsToBoldArray(i, 1))
-
-                Debug.Print ("do the thing")
-                cellVal.
+                Debug.Print (cellWordsArr(i))
             Next i
 
+            boldedCellString = Join(cellWordsArr)
+
+            Debug.Print ("meh")
+
+'            ' read each char in a range
+'            For i = 1 To Len(Cells(rowIndex, colIndex).Value)
+'
+'                If Cells(rowIndex, colIndex).Characters(i, 1).Font.FontStyle = "Bold" Then
+'                    currChar = Cells(rowIndex, colIndex).Characters(i, 1).Text
+'                    'Debug.Print (currChar & "if bold")
+'                    If arrayCounter >= 5000 Then
+'                        Debug.Print ("*Someone fit 5000 individually Bolded strings into a Cell! Wow!*")
+'                        Exit Sub
+'                    End If
+
+'                    If foundBoldStart = False Then
+'                        Debug.Print (currChar & "found start")
+'                        foundBoldStart = True
+'                        ' because we just found a Bold word,
+'                        ' we want to remember the start_index and the end_index
+'                        start_index_of_stringToBold = i
+'                        end_index_of_stringToBold = i
+'                        boldedCellVal = boldedCellVal & "*"
+'''                    If foundBoldStart = False Then
+'''                        foundBoldStart = True
+'''                        ' because we just found a Bold word,
+'''                        ' we want to remember the start_index and the end_index
+'''                        start_index_of_stringToBold = i
+'''                        end_index_of_stringToBold = i
+'                    End If
+'                    If foundBoldStart = True Then
+'                        stringToBold = stringToBold & currChar
+'                        boldedCellVal = boldedCellVal & currChar
+'                        ' because the Bold word was previously found
+'                        ' we want to keep track of the end index
+'                        end_index_of_stringToBold = i
+'                        stringsToBoldArray(arrayCounter, 0) = stringToBold
+'                        stringsToBoldArray(arrayCounter, 1) = start_index_of_stringToBold
+'                        stringsToBoldArray(arrayCounter, 2) = end_index_of_stringToBold
+'                    End If
+'                    'Debug.Print (stringToBold)
+'                Else
+'                  ' The bold word ended before the end of the string
+'                  ' lets reset out variables and prepare for the next Bold word
+'                  If stringToBold <> "" Then
+'''                      Debug.Print ("*" & stringsToBoldArray(arrayCounter, 0) & "*")
+'''                      Debug.Print ("start = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(arrayCounter, 1), 1).Text)
+'''                      Debug.Print ("end   = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(arrayCounter, 2), 1).Text)
+'''                      Debug.Print ("diff  = " & stringsToBoldArray(arrayCounter, 2) - stringsToBoldArray(arrayCounter, 1))
+'                      foundBoldStart = False ' reset to False until next bold
+'                      boldedCellVal = boldedCellVal & "*"
+'                      arrayCounter = arrayCounter + 1
+'                      stringToBold = ""      ' reset to empty string until next bold
+'                      start_index_of_stringToBold = 0
+'                      end_index_of_stringToBold = 0
+'                  End If
+'                End If
+'                'Debug.Print (currChar)
+'            Next i
+'''            If stringToBold <> "" Then
+'''                ' This if condition is useful when the Bold word
+'''                ' ends as the final word in the string
+'''                ' e.g: *Click* the button, *quickly!*
+'''                Debug.Print ("*" & stringToBold & "*")
+'''                Debug.Print ("start = " & Cells(rowIndex, colIndex).Characters(start_index_of_stringToBold, 1).Text)
+'''                Debug.Print ("end   = " & Cells(rowIndex, colIndex).Characters(end_index_of_stringToBold, 1).Text)
+'''                Debug.Print ("diff  = " & end_index_of_stringToBold - start_index_of_stringToBold)
+'''                foundBoldStart = False ' reset to False until next bold
+'''                stringToBold = ""      ' reset to empty string until next bold
+'''                start_index_of_stringToBold = 0
+'''                end_index_of_stringToBold = 0
+'''            End If
+'
+'            'For i = 1 To Len(Range("A1").Value)
+'            '  If Range("A1").Characters(i, 1).Font.FontStyle = "Bold" Then
+'            '    If foundBoldStart = False Then
+'            '        foundBoldStart = True
+'            '    End If
+'            '    If foundBoldStart = True Then
+'            '        stringToBold = stringToBold & Range("A1").Characters(i, 1).Text
+'            '    End If
+'            '    Debug.Print (stringToBold)
+'            '
+'            '    'Debug.Print ("The " & Range("A1").Characters(i, 1).Text & " character is bold.")
+'            '  End If
+'            'Next i
+
+
+'            'replace empty value with space to generate the cell border
+'            cellVal = currCol.Value
+'            boldedCellVal = ""
+
+
+'            For i = 0 To Len(cellVal)
+'                For k = 0 To arrayCounter
+'                    startIndexOfBold = stringsToBoldArray(k, 1)
+'                    If i = startIndexOfBold Then
+'                        boldedCellVal = boldedCellVal & "*" & currCol.Characters(i, 1).Text
+'
+ ''                   Else
+ '                   End If
+'                Next k
+'
+'            Next i
+'''
+'
+'            For i = 0 To arrayCounter
+'                ' This if condition is useful when the Bold word
+'                ' ends as the final word in the string
+'                ' e.g: *Click* the button, *quickly!*
+'
+'
+'
+'                If stringsToBoldArray(i, 1) = 1 Then
+'                    ' this Bold starts at the first character of cellVal
+'                    locationOfStartOfCurrBold = InStr(stringsToBoldArray(i, 1), cellVal, stringsToBoldArray(i, 0))
+'                    currBold = Left(cellVal, locationOfStartOfCurrBold)
+'                    theNext = Right(cellVal, Len(cellVal) - locationOfStartOfCurrBold)
+'                    currBold = "*" & currBold & "*"
+'                   ' theRest = "*" & theRest
+'                    boldedCellVal = boldedCellVal & currBold
+'                Else
+'
+'                    endIndexOfLastBold = stringsToBoldArray(i - 1, 2)
+'                    startIndexOfCurrBold = stringsToBoldArray(i, 1)
+'
+'                    meh = Left(cellVal, startIndexOfCurrBold)
+'
+'                    locationOfCurrBold = InStr(stringsToBoldArray(i, 1), cellVal, stringsToBoldArray(i, 0))
+'                    currBold = Left(cellVal, Len(cellVal) - locationOfCurrBold)
+'                    theRest = Mid(cellVal, Len(cellVal) - locationOfCurrBold)
+'                    currBold = "*" & currBold
+'                    theRest = "*" & theRest
+'                    boldedCellVal = currBold & theRest
+'                End If
+'
+'                Debug.Print (currBold)
+'                Debug.Print (theRest)
+'                Debug.Print ("*" & stringsToBoldArray(i, 0) & "*")
+'                Debug.Print ("start = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(i, 1), 1).Text)
+'                Debug.Print ("end   = " & Cells(rowIndex, colIndex).Characters(stringsToBoldArray(i, 2), 1).Text)
+'                Debug.Print ("diff  = " & stringsToBoldArray(arrayCounter, 2) - stringsToBoldArray(i, 1))
+'            Next i
+'
 ''            If stringToBold <> "" Then
 ''                ' This if condition is useful when the Bold word
 ''                ' ends as the final word in the string
@@ -151,6 +250,7 @@ Sub ConvertToJiraTable()
 ''            End If
 
 
+            cellVal = boldedCellString
 
             If (cellVal = "") Then cellVal = " "
             If rowIndex = 1 Then ' if it's the first row do the "||"
